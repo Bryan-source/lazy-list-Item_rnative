3f966936989130fa0b76b644448ffe245f95fb89
Bryan-source <bescobar34@yahoo.com>
2022-06-24 09:33:19 -0500

    Implement List.js

--- List.js
+++ List.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {Text, FlatList} from 'react-native';
+import styles from "./styles";
 
-function List(props) {
+function List({data, fetchItems, refreshItems, isRefreshing}) {
     return (
-        <div>
-            
-        </div>
+        <FlatList
+            data= {data}
+            renderItem = {({item}) => <Text style={styles.item}>{item.value}</Text>}
+            onEndReached = {fetchItems}
+            onRefresh = {refreshItems}
+            refreshing = {isRefreshing}
+        />
     );
+}
+
+List.propTypes = {
+    data: PropTypes.array.isRequired,
+    fetchItems: PropTypes.func.isRequired,
+    refreshItems: PropTypes.func.isRequired,
+    isRefreshing: PropTypes.bool.isRequired,
 }
 
 export default List;
